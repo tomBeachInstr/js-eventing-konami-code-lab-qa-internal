34d06ec525f003eb7d5d22e8dfd09ed2a924417b
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-19 23:52:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
   console.log("== init ==");
-  console.log("document.body:", document.body);
-  document.body.addEventListener("keydown", function() {
-    console.log("== keydown ==");
-  });
+
+  document.body.addEventListener('keydown', function() { onKeyDownHandler(e) }, false);
+
+  function onKeyDownHandler(e) {
+    console.log("== onKeyDownHandler ==");
+  }
 
 }
